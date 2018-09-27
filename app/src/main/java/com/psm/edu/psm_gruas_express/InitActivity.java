f46763649ae62bda7282b564aba290f750a23725
Angel Eduardo Vazquez Garcia <angel.eduardo.v.g@hotmail.com>
2018-09-27 15:46:05 -0500

    Fase de diseño
    Se aplicaron diseños al proyecto, aun le falta.

--- app/src/main/java/com/psm/edu/psm_gruas_express/InitActivity.java
+++ app/src/main/java/com/psm/edu/psm_gruas_express/InitActivity.java
@@ -3,6 +3,8 @@ package com.psm.edu.psm_gruas_express;
 import android.app.FragmentManager;
 import android.content.DialogInterface;
 import android.os.Build;
+import android.support.annotation.NonNull;
+import android.support.design.widget.NavigationView;
 import android.support.v4.app.Fragment;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
@@ -11,6 +13,7 @@ import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.view.Gravity;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
@@ -18,39 +21,50 @@ import android.widget.Toast;
 
 import com.psm.edu.psm_gruas_express.fragments.FragmentChat;
 import com.psm.edu.psm_gruas_express.fragments.FragmentMain;
+import com.psm.edu.psm_gruas_express.fragments.FragmentMap;
+import com.psm.edu.psm_gruas_express.fragments.FragmentMyService;
+import com.psm.edu.psm_gruas_express.fragments.FragmentServices;
+import com.psm.edu.psm_gruas_express.fragments.FragmentSettings;
+import com.psm.edu.psm_gruas_express.fragments.FragmentStateEmergency;
 
 public class InitActivity extends AppCompatActivity {
     DrawerLayout drawerLayout;
     Toolbar toolbarBottom;
     Toolbar toolbarActionBar;
     ActionBarDrawerToggle mdrawerToggle;
+    NavigationView navigationView;
     String TAG="";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_init);
-
+        //Find id view
         drawerLayout = (DrawerLayout) findViewById(R.id.idDrawerLayout);
         toolbarActionBar = (Toolbar) findViewById(R.id.toolbar_top);
         toolbarBottom = (Toolbar) findViewById(R.id.toolbar_bottom);
+        navigationView = (NavigationView) findViewById(R.id.nav_panel);
+
+        //Navegation bar icon burger
         setSupportActionBar(toolbarActionBar);
         mdrawerToggle = new ActionBarDrawerToggle(this, drawerLayout,toolbarActionBar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
         getSupportActionBar().setTitle("");
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         getSupportActionBar().setHomeButtonEnabled(true);
         mdrawerToggle.syncState();
-        if(mdrawerToggle.isDrawerSlideAnimationEnabled()){
-            Toast.makeText(this, "Funciono",Toast.LENGTH_LONG).show();
-        }
-        toolbarBottom.inflateMenu(R.menu.toolbar_shortcut);
 
+        NavEventMenuItemView();
+        ToolbarBottomEvent();
+
+        //Menu bottom
+       // toolbarBottom.inflateMenu(R.menu.toolbar_shortcut);
+/*
         mdrawerToggle.setToolbarNavigationClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 drawerLayout.openDrawer(GravityCompat.START);
             }
         });
-
+*/
 
 
         if (TAG.equals(""))
@@ -128,5 +142,64 @@ public class InitActivity extends AppCompatActivity {
                 })
                 .setIcon(android.R.drawable.ic_dialog_info)
                 .show();
+    }
+
+    private void NavEventMenuItemView() {
+        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+
+                switch(item.getItemId()) {
+                    case R.id.idInicio:
+                        changeFragment(new FragmentMain(),FragmentMain.TAG);
+                        break;
+                    case R.id.idMiServicio:
+                        changeFragment(new FragmentMyService(),FragmentMyService.TAG);
+                        break;
+                    case R.id.idServicios:
+                        changeFragment(new FragmentServices(),FragmentServices.TAG);
+                        break;
+                    case R.id.idMaps:
+                        changeFragment(new FragmentMap(), FragmentMap.TAG);
+                        break;
+                    case R.id.idChat:
+                        changeFragment(new FragmentChat(),FragmentChat.TAG);
+                        break;
+                    case R.id.idSettings:
+                        changeFragment(new FragmentSettings(), FragmentSettings.TAG);
+                        break;
+                    case R.id.idSalir:
+                        LogOut();
+                        break;
+                    default:
+
+                }
+                drawerLayout.closeDrawers();
+                return false;
+            }
+        });
+    }
+
+    private void ToolbarBottomEvent() {
+        toolbarBottom.inflateMenu(R.menu.toolbar_shortcut);
+        toolbarBottom.setOnMenuItemClickListener(new Toolbar.OnMenuItemClickListener() {
+            @Override
+            public boolean onMenuItemClick(MenuItem item) {
+
+                switch(item.getItemId()) {
+                    case R.id.idHome:
+                        changeFragment(new FragmentMain(),FragmentMain.TAG);
+                        return true;
+                    case R.id.idSearch:
+                        Toast.makeText(getApplicationContext(),"Gestura",Toast.LENGTH_SHORT).show();
+                        return true;
+                    case R.id.idChat:
+                        changeFragment(new FragmentChat(),FragmentChat.TAG);
+                        return true;
+                    default:
+                            return true;
+                }
+            }
+        });
     }
 }
